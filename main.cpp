e7fed942eddf528c26fbc6444b14331d324c4d40
Maki Yamazaki <nano@tomochan.home>
2020-02-02 11:51:29 +0000

    This should be a good tilemap

--- main.cpp
+++ main.cpp
@@ -12,20 +12,16 @@ class Test_State : public State
 public:
     bool init() override
     {
-        engine()->play_music("/home/nano/Music/ev.exi/Forget/04 Forget.mp3");
-        text = engine()->create_text("Assets/Fonts/Laconic.otf", 64, "Hello, My Text!");
-        return true;
+        
     }
     void update() override {}
     void render() override
     {
-        engine()->render_texture(my_img, 10, 10);
-        engine()->render_text(text, 300, 30);
+
     }
     void on_keydown(Si32 key) override
     {
-        if (key == SDLK_ESCAPE) engine()->switch_state("test2");
-        if (key == SDLK_SPACE) engine()->stop_music();
+
     }
 private:
     Text_ID text;
--- tilemap.h
+++ tilemap.h
@@ -1,17 +1,37 @@
 #ifndef _tilemap_h
 #define _tilemap_h
 
-template <class T, Ui w, Ui h>
+template <class T>
 struct Tilemap
 {
-    T* tile(Ui x, Ui y)
+public:
+    Tilemap(Ui width, Ui height) : w(width), h(height) { _data.reserve(w*h); }
+
+    Tilemap(Ui width, Ui height, const T & t) : Tilemap(w,h){ fill(t); }
+
+    void fill(const T & t)
     {
-        if ((x >= w) || (y >= h)) return nullptr;
-        else return data[y*w+x];
+        for (auto & it: data) it = t;
     }
-    T data[w*h];
-    const Ui WIDTH = w;
-    const Ui HEIGHT = h;
+
+    T* get(Ui x, Ui y)
+    {
+        if ((x >= WIDTH) || (y >= HEIGHT)) return nullptr;
+        else return _data[y*WIDTH +x];
+    }
+
+    void set(Ui x, Ui y, const T & t)
+    {
+        if ((x >= WIDTH) || (y >= HEIGHT)) return nullptr;
+        else _data[y*WIDTH + x] = t;
+    }
+
+    Ui width(){ return w; }
+    Ui height(){ return h; }
+private:
+    std::vector<T> _data;
+    Ui w;
+    Ui h;
 };
 
 #endif /* _tilemap_h */
