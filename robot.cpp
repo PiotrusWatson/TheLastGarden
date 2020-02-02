f24182d2a970d4b8b675b9e52771fce86e4eb8b2
Nicolae <nicolae.marton@gmail.com>
2020-02-02 14:42:14 +0000

    Push it We''re Done

--- robot.cpp
+++ robot.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <stdlib.h>
 #include "robot.hpp"
 #include "tiles.hpp"
 #include "tilemap.h"
@@ -17,19 +18,23 @@ void Robot::move(int direction, Tilemap<Ground> & g, Tilemap<Plant> & p, Tilemap
 
     switch(direction){
         case Direction::UP: 
-            if (oldDir == direction && isWalkable(ground, plant, obstacle)) x -= 1;  
+            if (oldDir == direction && isWalkable(ground, plant, obstacle)) 
+            {
+                x -= 1; 
+                count++;
+            }  
             dirx = x-1; diry = y;
             break;
         case Direction::DOWN: 
-            if (oldDir == direction && isWalkable(ground, plant, obstacle)) x += 1;
+            if (oldDir == direction && isWalkable(ground, plant, obstacle)){ x += 1;count++;}
             dirx = x+1; diry = y;
             break;
         case Direction::LEFT: 
-            if (oldDir == direction && isWalkable(ground, plant, obstacle)) y -= 1;
+            if (oldDir == direction && isWalkable(ground, plant, obstacle)) {y -= 1; count++;}
             dirx = x; diry = y-1;
             break; 
         case Direction::RIGHT: 
-            if (oldDir == direction && isWalkable(ground, plant, obstacle)) y += 1;
+            if (oldDir == direction && isWalkable(ground, plant, obstacle)) {y += 1;count++;}
             dirx = x; diry = y+1;
             break; 
         default:
@@ -41,7 +46,31 @@ void Robot::move(int direction, Tilemap<Ground> & g, Tilemap<Plant> & p, Tilemap
         oldDir = direction;
     }
     
-
+    if(count>5){
+        for(int i=0; i<11; i++){
+            for(int j=0; j<11; j++){
+                switch(*(p.get(i, j)))
+                {
+                    case Plant::LANDSEED: 
+                        p.set(i,j,Plant::LANDSMALL);
+                        break;
+                    case Plant::LANDSMALL: 
+                        p.set(i,j,Plant::LANDBIG);
+                        break;
+                    case Plant::WATERSEED: 
+                        p.set(i,j,Plant::WATERSMALL);
+                        break;
+                    case Plant::WATERSMALL: 
+                        p.set(i,j,Plant::WATERBIG);
+                        break;
+                    default:
+                    break;
+                    
+                }
+            }    
+        }
+        count = 0;
+    }
     
 };
 
@@ -55,6 +84,15 @@ void Robot::plantTree(Tilemap<Ground> & g, Tilemap<Plant> & p)
 {
     Ground ground = *(g.get(diry, dirx));
 
-    if(ground == Ground::ALIVE) p.set(diry, dirx, Plant::LANDSEED);
+
+
+    if(ground == Ground::ALIVE)
+    {
+        int random = rand()%2;
+
+        random == 1 ? p.set(diry, dirx, Plant::LANDSEED) : p.set(diry, dirx, Plant::WATERSEED);  
+        
+    }
 }
+
 
--- robot.hpp
+++ robot.hpp
@@ -14,12 +14,13 @@ struct Robot
         UP, DOWN, LEFT, RIGHT, NONE
     };
 
-    Robot() : x(9), y(2), energy(20), seeds(3), oldDir(NONE), dir(RIGHT){}
+    Robot() : x(9), y(2), energy(20), seeds(3), oldDir(NONE), dir(RIGHT), count(0){}
     Robot(int xPos, int yPos) : x(xPos), y(yPos), energy(0) {}
     
     int x, y, energy, seeds;
     int dirx = 0, diry = 0;
     int dir, oldDir;
+    int count;
 
     void move(int direction, Tilemap<Ground> & g, Tilemap<Plant> & p, Tilemap<Obstacle> & o);
 
