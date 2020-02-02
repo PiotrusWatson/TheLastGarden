eb19b0e2f8e1cb349d64da0d100c2baccce52de5
Maki Yamazaki <nano@tomochan.home>
2020-02-02 13:00:53 +0000

    engine improvements

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "state.h"
 #include "animation.hpp"
 #include "tilemap.h"
+#include "debug.h"
 
-std::string my_img = "/home/nano/Pictures/Avatars/yuriheart.jpg";
-
+std::string title_img = "Assets/Images/Title_Screen.png";
 
 
 class Test_State : public State
@@ -17,7 +17,16 @@ public:
     void update() override {}
     void render() override
     {
-        engine()->render_texture("Assets/Images/Title_Screen.png",0,0);
+        int img_w = 0, img_h = 0, win_w = 0, win_h = 0;
+        engine()->get_texture_size(title_img, &img_w, &img_h);
+
+        engine()->get_window_size(&win_w, &win_h);
+        if (img_w > 0)
+        {
+            float scale = float(win_w)/float(img_w);
+            engine()->render_texture(title_img,0,0, scale, scale);
+        }
+        else MX_LOG(img_w << "x" << img_h);
     }
     void on_keydown(Si32 key) override
     {
@@ -29,7 +38,7 @@ private:
 
 int main(int argc, const char * argv[])
 {
-    Engine engine("The Last Garden", 800, 450, Engine::WINDOWED);
+    Engine engine("The Last Garden", 800, 450, Engine::FULLSCREEN);
     Test_State state;
 
     engine.push_state("test", &state);
