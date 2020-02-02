6b1c120dd27f0f14ee67387702d6295f34eb0525
Maki Yamazaki <nano@tomochan.home>
2020-02-02 11:30:59 +0000

    Now with Tilemaps

--- tilemap.h
+++ tilemap.h
@@ -1,44 +1,17 @@
 #ifndef _tilemap_h
 #define _tilemap_h
 
-#include <vector>
-#include "pos2d.h"
-
-template <class T>
-class Tilemap
+template <class T, Ui w, Ui h>
+struct Tilemap
 {
-public:
-    Tilemap(Ui w, Ui h, const std::vector<T> & data) : width(w), height(h), storage(data) {}
-
-    T* get_tile(Ui x, Ui y)
+    T* tile(Ui x, Ui y)
     {
-        if (x >= width || y >= height) return nullptr;
-        Ui pos = get_index(x, y);
+        if ((x >= w) || (y >= h)) return nullptr;
+        else return data[y*w+x];
     }
-
-    bool locate(T* t, Ui * xOut, Ui * yOut)
-    {
-        if (xOut == nullptr || yOut == nullptr) return false;
-        for (int y = 0; y < height; ++y) 
-        {
-            for (int x = 0; x < width; ++x)
-            {
-                if (t == &storage[get_index(x,y)])
-                {
-                    *xOut = x;
-                    *yOut = y;
-                    return true;
-                }
-            }
-        }
-    }
-private:
-    Ui get_index(Ui x, Ui y)
-    {
-        return y*width + x;
-    }
-    std::vector<T> storage;
-    const int width, height;
+    T data[w*h];
+    const Ui WIDTH = w;
+    const Ui HEIGHT = h;
 };
 
 #endif /* _tilemap_h */
